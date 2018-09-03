5e3d39425dfcce09b65060cd33f5732957f55f84
Zalron <33308793+Zalron@users.noreply.github.com>
2018-09-03 15:03:26 +1000

    Adding Unity project
    Added one script and beginning to work on it

---
+++ Prototype_Dreamer_Conquest_Unity_Project/Assets/Scripts/World.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorldManager
+{
+    public class World : MonoBehaviour
+    {
+        public int chuckWidth = 20, chunkHeight = 20, seed = 0; // ints for chuck width height and seed
+        void Start() // Use this for initialization
+        {
+            if (seed == 0)
+            {
+                seed = Random.Range(0, int.MaxValue);
+            }
+        }
+        void Update() // Update is called once per frame
+        {
+
+        }
+    }
+}
