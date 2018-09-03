e8ce1c36fc7bba718b99a0e8942ef7d97479e716
Zalron <33308793+Zalron@users.noreply.github.com>
2018-09-03 18:09:42 +1000

    Imported the simplexnoise script and implemented into the chunk script
    Also started working on the player camera

--- Prototype_Dreamer_Conquest_Unity_Project/Assets/Scripts/Chunk.cs
+++ Prototype_Dreamer_Conquest_Unity_Project/Assets/Scripts/Chunk.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimplexNoise;
 namespace WorldManager
 {
     [RequireComponent(typeof(MeshFilter))] // automatically adding the MeshFilter component 
@@ -22,12 +23,23 @@ namespace WorldManager
             meshRenderer = GetComponent<MeshRenderer>(); // setting the mesh renderer to the component
             meshCollider = GetComponent<MeshCollider>(); // setting the mesh collider to the component
             map = new int[World.currentWorld.chunkWidth, World.currentWorld.chunkHeight, World.currentWorld.chunkWidth]; // setting the 3 dimensional map array to the map variable in here
-            for (int x = 0; x< World.currentWorld.chunkWidth; x++) // building the world at the start of runtime
+            for (int x = 0; x< World.currentWorld.chunkWidth; x++) // building the world at the start of runtime and using SimplexNoise to generate the noise for random terrian
             {
-                for (int z = 0; z < World.currentWorld.chunkWidth; z++)
+                float noiseX = (float)x / 20;
+                for (int y = 0; y < World.currentWorld.chunkHeight; y++)
                 {
-                    map[x, 0, z] = 1;
-                    map[x, 1, z] = Random.Range(0, 2);
+                    float noiseY = (float)y / 20;
+                    for (int z = 0; z < World.currentWorld.chunkWidth; z++)
+                    {
+                        float noiseZ = (float)z / 20;
+                        float noiseValue = Noise.Generate(noiseX, noiseY, noiseZ);
+                        noiseValue += (10f - (float)y) / 10;
+                        //noiseValue /= (float)y / 5;
+                        if (noiseValue > 0.2f)
+                        {
+                            map[x, y, z] = 1;
+                        }
+                    }
                 }
             }
             CreateVisualMesh(); // calling the createvisualmesh to generate a chunk 
@@ -45,7 +57,7 @@ namespace WorldManager
             List<int> tris = new List<int>(); // list of triangles on the mesh
             for (int x = 0; x < World.currentWorld.chunkWidth; x++) // building the world at the start of runtime
             {
-                for (int y = 0; y < World.currentWorld.chunkWidth; y++)
+                for (int y = 0; y < World.currentWorld.chunkHeight; y++)
                 {
                     for (int z = 0; z < World.currentWorld.chunkWidth; z++)
                     {
@@ -129,8 +141,7 @@ namespace WorldManager
                     return true;
                 case 1:
                     return false;
-            }
-                
+            } 
         }
         public virtual int GetInt(int x, int y, int z) // gets the int 
         {
